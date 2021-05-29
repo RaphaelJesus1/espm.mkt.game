e30a187a9965cef973eec98377c186db3f3689a0
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2021-05-29 02:02:51 -0300

    Question panel pushing the screen - animation
    + some css adjust

--- public/index.js
+++ public/index.js
@@ -1,9 +1,22 @@
 /* Set the width of the side navigation to 250px */
 function openNav(id) {
-    document.getElementById(id).style.width = "900px";
+    document.getElementById(id).style.width = "300px";
 }
   
 /* Set the width of the side navigation to 0 */
 function closeNav(id) {
     document.getElementById(id).style.width = "0";
 }
+
+/* Abrir e fechar o painel de questões */
+function openQuestions() {
+    document.getElementById("question-container").style.height = "20vh";
+    // document.querySelector(".tela-quiz").style.marginBottom = "250px";
+    document.querySelector(".tela-quiz").style.height = "80vh";
+  }
+  
+  function closeQuestions() {
+    document.getElementById("question-container").style.height = "0";
+    // document.querySelector(".tela-quiz").style.marginBottom = "0";
+    document.querySelector(".tela-quiz").style.height = "100vh";
+  }
